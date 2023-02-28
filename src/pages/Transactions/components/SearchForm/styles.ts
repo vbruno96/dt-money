dc5fd5c5b243efe94e64fd992808d3cf37f3b986
Bruno Vinícius <vbruno96@gmail.com>
2023-02-28 18:34:49 -0300

    create transactions search

---
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+
+export const SearchFormContainer = styled.form`
+  display: flex;
+  gap: 1rem;
+  margin-bottom: 1.5rem;
+
+  input {
+    flex: 1;
+    background: ${(props) => props.theme['gray-900']};
+    color: ${(props) => props.theme['gray-300']};
+    border: none;
+    border-radius: 6px;
+    padding: 1rem;
+
+    &::placeholder {
+      color: ${(props) => props.theme['gray-500']};
+    }
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+    background: transparent;
+    color: ${(props) => props.theme['green-300']};
+    border: 1px solid ${(props) => props.theme['green-300']};
+    border-radius: 6px;
+    padding: 1rem;
+    font-weight: 700;
+    transition: all 0.4s;
+
+    &:hover {
+      background: ${(props) => props.theme['green-500']};
+      color: ${(props) => props.theme.white};
+      border: 1px solid ${(props) => props.theme['green-500']};
+    }
+  }
+`
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,48 @@
 import { Header } from '../../components/Header'
-import {} from './styles'
+import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
+import {
+  PriceHighLight,
+  TransactionTable,
+  TransactionsContainer,
+} from './styles'
 
 export function Transactions() {
   return (
     <>
       <Header />
+      <Summary />
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.800,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>27/02/2023</td>
+            </tr>
+            <tr>
+              <td>Hamburguer</td>
+              <td>
+                <PriceHighLight variant="outcome">-R$ 32,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>25/02/2023</td>
+            </tr>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.800,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>22/02/2023</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionsContainer>
     </>
   )
 }
